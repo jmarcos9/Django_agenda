7d8b74f10edbc054ada77c873ea6c40c7c6de512
jmarcos9 <66381477+jmarcos9@users.noreply.github.com>
2021-06-25 21:22:58 -0300

    inserindo formulário no dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages, auth
 from django.core.validators import validate_email
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required #para não possibiçitar aceso a janela anonima o dashboard
-
+from .models import FormContato
 
 def login(request):
 
@@ -81,4 +81,17 @@ def cadastro(request):
 
 @login_required(redirect_field_name='login')
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+        if request.method != 'POST':
+            form = FormContato
+            return render(request, 'accounts/dashboard.html', {'form': form})
+
+        form = FormContato(request.POST, request.FILES)
+
+        if not form.is_valid():
+            messages.error(request, 'Err ao enviar formulário')
+            form = FormContato(request.POST)
+            return render(request, 'accounts/dashboard.html', {'form': form})
+
+        form.save()
+        messages.success(request, f'Contato {request.POST.get("nome")} salvo com sucesso!')
+        return redirect('dashboard')
